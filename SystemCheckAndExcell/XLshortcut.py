603034fb9593a6ae3aa20a1640d220cf1e5aaf83
Laxman Koirala <nirlax10000BC>
2021-01-20 00:25:20 +0200

    error fixed

--- SystemCheckAndExcell/XLshortcut.py
+++ SystemCheckAndExcell/XLshortcut.py
@@ -16,10 +16,13 @@ def readData(file,sheetName,rownum,columnnum):
     sheet =  workbook.get_sheet_by_name(sheetName)
     return sheet.cell(row=rownum,column=columnnum).value
 
-def writeData(file1,sheetName,rownum,columnnum,data,file2):
-    workbook = openpyxl.load_workbook(file1)
+def writeData(file,sheetName,rownum,columnnum,data):
+    workbook = openpyxl.load_workbook(file)
     sheet =  workbook.get_sheet_by_name(sheetName)
     sheet.cell(row=rownum,column=columnnum).value = data
-    workbook.save(file2)
+    workbook.save(file)
 
+def copyoriginal(original,duplicate):
+    workbook = openpyxl.load_workbook(original)
+    workbook.save(duplicate)
 
--- SystemCheckAndExcell/systemtestrun.py
+++ SystemCheckAndExcell/systemtestrun.py
@@ -3,28 +3,30 @@ import pathlib
 import os
 
 def files(file,file1,rows,*args):
-    if args:
-        file = file1
+    if not args:
+        XLshortcut.copyoriginal(file,file1)
     for r in range(2, rows + 1):
-        Testcase = str(XLshortcut.readData(file, 'Sheet1', r, 1))
-        Status = XLshortcut.readData(file, 'Sheet1', r, 2)
-        RunNoR = (XLshortcut.readData(file, 'Sheet1', r, 3))
+        Testcase = str(XLshortcut.readData(file1, 'Sheet1', r, 1))
+        Status = XLshortcut.readData(file1, 'Sheet1', r, 2)
+        RunNoR = (XLshortcut.readData(file1, 'Sheet1', r, 3))
         if Status != 'Passed':
-            base_dir = str(pathlib.PureWindowsPath(r'C:\Testing_internship\QA_internship'))
+            base_dir = str(pathlib.PureWindowsPath(r'C:\Testing_internshi\QA_internship'))
             Pythonfilepath = os.path.join(base_dir, Testcase)
             print(Pythonfilepath)
 
             try:
                 exec(open(Pythonfilepath).read())
-                XLshortcut.writeData(file, 'Sheet1', r, 2, 'Passed', file1)
+                XLshortcut.writeData(file1, 'Sheet1', r, 2, 'Passed')
             except:
-                XLshortcut.writeData(file, 'Sheet1', r, 2, 'Failed', file1)
-                XLshortcut.writeData(file, 'Sheet1', r, 3, RunNoR + 1, file1)
-                pass
+                XLshortcut.writeData(file1, 'Sheet1', r, 2, 'Failed')
+                XLshortcut.writeData(file1, 'Sheet1', r, 3, RunNoR + 1)
+
+
+
 
 file= "SystemTestCasesCheckup.xlsx"  #path
-file1 = "DuplicateSystemCheckup1.xlsx"
+file1 = "DuplicateSystemCheckup2.xlsx"
 rows = XLshortcut.getTotalrows(file, 'Sheet1')
 files(file,file1,rows)
-files(file,file1,rows,2) # args can be anything
+files(file,file1,rows,2)
 
